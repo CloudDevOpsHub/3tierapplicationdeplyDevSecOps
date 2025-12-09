3b7b4d84a627a971f3167ff90466d2874a3b1b4a
CloudDevOpsHub <100401787+CloudDevOpsHub@users.noreply.github.com>
2025-12-09 19:16:52 +0530

    Update 1st-Jenkins.sh

--- 1st-Jenkins.sh
+++ 1st-Jenkins.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# Install OpenJDK 17 JRE Headless
+# Install JRE
 sudo apt install openjdk-17-jre-headless -y
+
+# Create keyrings folder
+sudo mkdir -p /usr/share/keyrings
 
 # Download Jenkins GPG key
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
 
-# Add Jenkins repository to package manager sources
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+# Add Jenkins repo
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/" | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-# Update package manager repositories
-sudo apt-get update
+# Update
+sudo apt-get update -y
 
 # Install Jenkins
 sudo apt-get install jenkins -y
